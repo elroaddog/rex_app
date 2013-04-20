3b945313b06bdfb99048781c8481f9a94bdb9267
elroaddog <arodi.gonzalez@gmail.com>
2013-04-19 22:17:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RexApp::Application.config.secret_token = '76c038b806ae60df4df2dd1a3be0993f98f2962586c6ef64f9fc4e90fc14d8fa1855519d1b575149ec327978dbf9c2c92e30a6fb99ba72db05b881ea3dde0f30'
